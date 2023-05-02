5d2e6b969b48d319fd039600589f21b275440d39
Emilie Bronchain <emilie.bronchain28@gmail.com>
2023-05-02 10:00:18 +0200

    express quest commit

--- movieHandlers.js
+++ movieHandlers.js
@@ -33,12 +33,19 @@ const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+    database
+      .query("select * from movies where id = ?", [id])
+      .then(([movies]) => {
+        if (movies[0] != null) {
+          res.json(movies[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
 };
 
 module.exports = {
---
+++ usersHandlers.js
@@ -0,0 +1,36 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("SELECT * FROM users WHERE id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
+      } else {
+        res.status(404).send("User not found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
